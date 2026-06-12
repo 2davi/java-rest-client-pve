808a1cf200b5797e9099b8adbd82e55addfc5cfc
2davi <2davijean@gmail.com>
2026-06-12 16:24:32 +0900

    refactor: Node 정보 전역화. 커밋 누락사항 보충.

--- src/main/java/dev/the2davi/lab/api/dto/CmpTaskHistoryDto.java
+++ src/main/java/dev/the2davi/lab/api/dto/CmpTaskHistoryDto.java
@@ -1,8 +1,12 @@
 package dev.the2davi.lab.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record CmpTaskHistoryDto(
 		String upid				//TASK's UPID
-		, String resourceType	//VM, CT, NODE, ...
+		, String node
+		, @JsonProperty("resource-type")
+		  String resourceType	//VM, CT, NODE, ...
 		, String resourceId		//VMID, ...
 		, String action			//START, STOP, SHUTDOWN, ...
 		, String displayTitle	//프론트엔드 UI전용 이쁜 텍스트
